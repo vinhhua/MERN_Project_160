c704d768dcd2561e15c91139bc6aa985397fed7e
huaphuvinh <huaphuvinh@gmail.com>
2021-04-22 18:49:11 -0700

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,12 @@ import RegisterForm from "./components/Form/RegisterForm";
 import ForgotPasswordForm from "./components/Form/ForgotPasswordForm";
 import ResetPasswordForm from "./components/Form/ResetPasswordForm";
 import LoggedInForm from "./components/Form/LoggedInForm";
-import Navbar from "./components/Navbar/index";
 import AboutForm from "./components/Form/AboutForm";
 import ServicesForm from "./components/Form/ServicesForm";
 import ContactForm from "./components/Form/ContactForm";
 import SpendingForm from "./components/Form/SpendingForm"
 import "./App.css";
+import { LandingPage } from "./components/Form/LandingPage";
 
 const App = () => {
   return (
@@ -22,13 +22,13 @@ const App = () => {
       <div className="app">
         <Switch>
           {/* <PrivateRoute exact path="/" component={LoggedInForm} /> */}
-          <Route exact path="/"></Route>
+          <Route exact path="/" component={LandingPage} />
           <Route exact path="/login" component={LoginForm} />
           <Route exact path="/register" component={RegisterForm} />
           <Route exact path="/about" component={AboutForm} />
           <Route exact path="/services" component={ServicesForm} />
           <Route exact path="/contact-us" component={ContactForm} />
-          <Route exact path="/spending"><SpendingForm /></Route>
+          <Route exact path="/spending" component={SpendingForm} />
           <Route
             exact
             path="/forgotpassword"
---
+++ client/src/components/Form/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+export const LandingPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1> Landing Page </h1>
+        </div>
+    )
+}
