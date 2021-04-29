1fafa398fb19b413bcdae60a8ee386bf88ce544b
huaphuvinh <huaphuvinh@gmail.com>
2021-04-28 23:38:03 -0700

    toggle login/logout button + style

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,4 +1,6 @@
+import { blue } from '@material-ui/core/colors';
 import React from 'react'
+import { useState } from 'react'
 import {
     Nav,
     NavLink,
@@ -7,8 +9,45 @@ import {
     NavBtn,
     NavBtnLink
   } from './NavbarElements';
-
+  
 const Navbar = () => {
+    const [loggedOut, setLoggedOut] = useState(false)
+    const logout = () => {
+        localStorage.clear();
+        setLoggedOut(true);
+    }
+    if (loggedOut) {
+        return (
+            <>
+                <Nav>
+                    <NavLink to="/">
+                        <h1>Logo</h1>
+                    </NavLink>
+                    <Bars />
+                    <NavMenu>
+                        <NavLink to="/about">
+                            <h1>About</h1>
+                        </NavLink>
+                        <NavLink to="/services">
+                            <h1>Services</h1>
+                        </NavLink>
+                        <NavLink to="/contact-us">
+                            <h1>Contact Us</h1>
+                        </NavLink>
+                        <NavLink to="/spending">
+                            <h1>Spending</h1>
+                        </NavLink>
+                        <NavLink to="/register">
+                            <h1>Sign Up</h1>
+                        </NavLink>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/login">Sign In</NavBtnLink>
+                    </NavBtn>
+                </Nav>
+            </>
+        )
+    }
     return (
         <>
             <Nav>
@@ -34,7 +73,18 @@ const Navbar = () => {
                     </NavLink>
                 </NavMenu>
                 <NavBtn>
-                    <NavBtnLink to="/login">Sign In</NavBtnLink>
+                    <button style={{
+                        backgroundColor: "#256ce1",
+                        padding: "10px 22px",
+                        outline: "none",
+                        border: "none",
+                        color: "#fff",
+                        borderRadius: "4px",
+                        cursor: "pointer",
+                        transition: "all 0.2s ease-in-out",
+                        textDecoration: "none"
+                    }}
+                    onClick={logout}>Sign Out</button>
                 </NavBtn>
             </Nav>
         </>
