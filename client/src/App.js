8c2fe3b9fb8caf2bb037b9f1473a52d03f2af1c2
Brian L <brianla7755@gmail.com>
2021-05-04 20:03:35 -0700

    removed forms
    removed about, service, & contact

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,6 @@ import RegisterForm from "./components/Form/RegisterForm";
 import ForgotPasswordForm from "./components/Form/ForgotPasswordForm";
 import ResetPasswordForm from "./components/Form/ResetPasswordForm";
 import LoggedInForm from "./components/Form/LoggedInForm";
-import AboutForm from "./components/Form/AboutForm";
-import ServicesForm from "./components/Form/ServicesForm";
-import ContactForm from "./components/Form/ContactForm";
 import SpendingForm from "./components/Form/SpendingForm"
 import "./App.css";
 import { LandingPage } from "./components/Form/LandingPage";
@@ -36,9 +33,6 @@ const App = () => {
           <Route exact path="/" component={LandingPage}/>
           <Route exact path="/login" component={LoginForm}/>
           <Route exact path="/register" component={RegisterForm}/>
-          <Route exact path="/about" component={AboutForm}/>
-          <Route exact path="/services" component={ServicesForm}/>
-          <Route exact path="/contact-us" component={ContactForm}/>
           <Route exact path="/forgotpassword" component={ForgotPasswordForm}/>
           <Route exact path="/passwordreset/:resetToken" component={ResetPasswordForm}/>
           <Provider store={store}>
