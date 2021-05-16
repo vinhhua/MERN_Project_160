ad645184f84449b7f8b6b588bca12682a019b6f3
Brian Huynh <brian.huynh02@sjsu.edu>
2021-05-16 03:23:33 -0700

    revising schema

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -6,18 +6,13 @@
 //  acquire mongoose
 const mongoose = require("mongoose");
 
-//  schema creation: title (String), message (String), creator (String), tags [String], selectedFile (String),
-//  likeCount: {type: Number, default: 0}, createdAt: {type: Date, default: new Date()}
+//  schema creation: title (String), message (String), creator (String), tags [String],
+//  createdAt: {type: Date, default: new Date()}
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
     creator: String,
     tags: [String],
-    selectedFile: String,
-    likeCount: {
-        type: Number,
-        default: 0,
-    },
     createdAt: {
         type: Date,
         default: new Date(),
